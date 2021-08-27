6ea682f7f003667c23025879c5f3e40866058a9e
Chiamaka Nwolisa <chiamakanwolisa@gmail.com>
2021-08-28 01:52:23 +0200

    Add extra dog service for practise (#3)
    * Add extra dog service for practise
    
    * Fix failing test

--- src/dogs/dogs.service.spec.ts
+++ src/dogs/dogs.service.spec.ts
@@ -1,20 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { DogsController } from './dogs.controller';
 import { DogsService } from './dogs.service';
 
-describe('DogsController', () => {
-  let controller: DogsController;
+describe('DogsService', () => {
+  let service: DogsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [DogsController],
       providers: [DogsService],
     }).compile();
 
-    controller = module.get<DogsController>(DogsController);
+    service = module.get<DogsService>(DogsService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(service).toBeDefined();
   });
 });
