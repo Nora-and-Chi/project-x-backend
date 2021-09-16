9ce463395f4aeeb74b7471c6e957c2ac77cf27d8
Chiamaka Nwolisa <cnwo@hellofresh.com>
2021-09-16 22:08:21 +0200

    Add user entity

---
+++ src/users/user.entity.ts
@@ -0,0 +1,19 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  login_token: string;
+
+  @Column()
+  login_token_verified_at: string;
+}
